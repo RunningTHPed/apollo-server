e71e73be5a31da0bcf25e22504150b9a1fd322d4
RunningTHPed <pednoiiz008@gmail.com>
2022-03-22 03:56:44 +0700

    setup ApolloServer and demo graphql

---
+++ index.js
@@ -0,0 +1,53 @@
+const {ApolloServer, gql} = require('apollo-server');
+
+const typeDefs = gql`
+
+    type User{
+        name: String
+        age: Int
+        position: String
+    }
+
+    type Query{
+        users:[User]
+    }
+    type Mutation{
+        createUser(name: String,age:String,position:String):User
+    }
+`
+let users = [
+    {
+        name:"Test",
+        age:"22",
+        position:"student"
+    },
+    {
+        name:"Cristiano Ronaldo",
+        age:"36",
+        position:"fooballer"
+    },
+    {
+        name:"Emma Stone",
+        age:"33",
+        position:"actress"
+    },
+]
+
+const resolvers = {
+    Query:{
+        users: () => users
+    },
+    Mutation:{
+        createUser(parent,args){
+            const newUser = args
+            users.push(newUser)
+            return newUser
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen().then(({url})=>{
+    console.log(`server ready at port ${url}`)
+})
